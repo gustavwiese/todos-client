34580b53a08b2dd2656e6c0da38458c4c6f6841e
Tomas Pesek <pesekt@gmail.com>
2023-11-23 10:30:15 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import NewTodo from "./components/NewTodo";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <>
+      <NewTodo />
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
